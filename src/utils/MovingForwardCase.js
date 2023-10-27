6345f3aed61618bf872c9aed2d82e167a2370ef6
hyj5076 <hyj5076@hanmail.net>
2023-10-27 22:31:23 +0900

    chore: 오타수정

--- src/utils/MovingForwardCase.js
+++ src/utils/MovingForwardCase.js
@@ -2,7 +2,7 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 import { FORWARD_MATCHING_POINT, FORWARD_DASH } from '../const/Messages';
 
 export const movingForwardCase = () => {
-  const number = MissionUtils.Random.picktrueNumberInRange(0, 9);
+  const number = MissionUtils.Random.pickNumberInRange(0, 9);
 
   if (number >= FORWARD_MATCHING_POINT) {
     Console.print(FORWARD_DASH);
