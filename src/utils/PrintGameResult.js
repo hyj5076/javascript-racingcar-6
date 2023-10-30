d3e0ba52ca964e848676ec38994682b0a9c64e91
hyj5076 <hyj5076@hanmail.net>
2023-10-31 02:16:03 +0900

    remove: 불필요한 파일 삭제

--- src/utils/PrintGameResult.js
+++
@@ -1,8 +0,0 @@
-import { GAME_RESULT, WINNER } from "../const/Messages";
-
-export const printGameResult = (carGame) => {
-  Console.print(GAME_RESULT);
-  carGame.printRoundResult();
-  const winners = carGame.findWinner();
-  Console.print(`${WINNER} : ${winners.join(", ")}`);
-};
